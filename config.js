6d1719b9d5191a267921feb6a7672eb95b09a312
hahakeke-ed <i25263698@gmail.com>
2025-09-06 10:47:59 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Google Apps Script 웹앱 URL (/exec로 끝나야 합니다)
-window.GAS_URL = "PUT_APPS_SCRIPT_EXEC_URL_HERE";
+window.GAS_URL = "https://script.google.com/macros/s/AKfycbwjnjwWKQg35zgDlLGa2W-vKa_azE9hO9WISZ5WCGTPhzyZ9tGmDfhv8mQmC4BlL9bC/exec";
