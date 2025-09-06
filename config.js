e6ae57ca3ca0f3656626df635ad6ec441e1c6691
hahakeke-ed <i25263698@gmail.com>
2025-09-06 10:18:44 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Google Apps Script 웹앱 끝점 URL을 여기에 붙여넣으세요.
-const GAS_URL = ""; // 예: 'https://script.google.com/macros/s/AKfycbx.../exec'
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwntuRSeRlSxwzG8MuNDT4c0EVaPulwDMwfLsMMlU6v9AytDXsDkIJ82o1STpJ2aUy8/exec"
