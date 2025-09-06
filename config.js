98bd464d43e849de4b541a31bfab3235e6618c32
hahakeke-ed <i25263698@gmail.com>
2025-09-06 10:40:50 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // GAS URL must end with /exec
-window.GAS_URL = "PUT_EXEC_URL_HERE";
+window.GAS_URL = "https://script.google.com/macros/s/AKfycbwjnjwWKQg35zgDlLGa2W-vKa_azE9hO9WISZ5WCGTPhzyZ9tGmDfhv8mQmC4BlL9bC/exec";
