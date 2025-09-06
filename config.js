fdd76d2428a0b2df88d84cd41589fd172df18287
hahakeke-ed <i25263698@gmail.com>
2025-09-06 10:40:14 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// Google Apps Script 웹앱 끝점 URL을 여기에 붙여넣으세요.
-window.GAS_URL =  GAS_URL = "https://script.google.com/macros/s/AKfycbwntuRSeRlSxwzG8MuNDT4c0EVaPulwDMwfLsMMlU6v9AytDXsDkIJ82o1STpJ2aUy8/exec"
+// GAS URL must end with /exec
+window.GAS_URL = "PUT_EXEC_URL_HERE";
